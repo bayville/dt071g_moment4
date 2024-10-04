ca68dfe5fc03332c44fe35c6fe2abd737d5e0acd
Fredrik <fredrik@bayville.se>
2024-10-04 13:19:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Load sample data
+using Moment4;
+
+var sampleData = new SentimentAnalysis.ModelInput()
+{
+    Col0 = @"Place is clean. ",
+};
+
+//Load model and predict output
+var result = SentimentAnalysis.Predict(sampleData);
+var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+Console.WriteLine(sentiment);
